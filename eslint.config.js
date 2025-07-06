b13f85e54d83b6b037c5e7e52a0a1770faffc1b6
Heather813903 <heathersmith9378@gmail.com>
2025-07-06 09:35:07 -0400

    checking completed todo app against AI reviewer for changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
+
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -22,8 +24,22 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+
+    settings: {
+      react: {
+        version: 'detect', 
+      },
+    },
+
     rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+
+
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'warn',
+      'react/prop-types': 'off',
     },
   },
 ])
