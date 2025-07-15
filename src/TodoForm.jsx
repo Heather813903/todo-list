edc74208ed53a9f4702e748bc57d03d9c88fcb12
Heather813903 <heathersmith9378@gmail.com>
2025-07-15 15:22:20 -0400

    fixing diverged branches

---
+++ src/TodoForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TodoForm() {
+    return (
+        <form>
+            <label htmlFor="todoTitle">Todo</label>
+            <input type="text" id="todoTitle" />
+            <button type="submit" style={{ backgroundColor: 'blue', color: 'white', fontSize: '16px'}}>Add Todo</button> 
+        </form>
+    );
+
+}
+
+export default TodoForm;
