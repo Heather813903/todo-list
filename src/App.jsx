7e1a5c21f11bb1489aac6988f6a75b27316fac85
Heather813903 <heathersmith9378@gmail.com>
2025-08-14 12:15:32 -0400

    Move TodoForm, TodoList, and TodoListItem into features directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css'
-import TodoForm from './TodoForm';
-import TodoList from './TodoList';
+import TodoForm from './features/TodoForm';
+import TodoList from './features/TodoList/TodoList.jsx';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
