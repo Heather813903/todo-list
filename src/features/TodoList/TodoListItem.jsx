c57673f8073c7274c32ce6f73efebf5085e25fa7
Heather813903 <heathersmith9378@gmail.com>
2025-09-12 16:30:34 -0400

    updated li in TodoListItem.jsx

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TextInputWithLabel from '../../shared/TextInputWithLabel';
 import styles from './TodoListItem.module.css';
 
